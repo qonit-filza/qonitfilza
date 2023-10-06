c115d45cc734c290bf40b58fe5f42b05a919848e
qonit-filza <109884831+qonit-filza@users.noreply.github.com>
2023-10-06 07:35:11 +0700

    initial setup & experiment code

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        retro: "5px 5px 0 0 rgba(0,0,0,0.75)",
+      },
+    },
+  },
+  plugins: [],
+};
